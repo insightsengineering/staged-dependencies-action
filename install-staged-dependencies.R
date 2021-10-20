66379504286f96d741ad0dc9cd22edec03c58424
knightdave <13086223+knightdave@users.noreply.github.com>
2021-10-20 07:46:58 +0000

    Fix typo

--- install-staged-dependencies.R
+++ install-staged-dependencies.R
@@ -13,16 +13,16 @@ cat(paste("Staged dependencies version:", sd_version, "\n"))
 os_info <- read.csv("/etc/os-release", sep = "=", header = FALSE)
 v_os_info <- setNames(os_info$V2, os_info$V1)
 if (v_os_info[['NAME']] == "Ubuntu") {
-ubuntu_version <- v_os_info[['VERSION_ID']]
-sys_deps_for_pkg <- remotes::system_requirements("ubuntu", ubuntu_version, path = repo_path)
-sys_pgks <- gsub("^apt-get install -y ", "", sys_deps_for_pkg)
-has_pkgs <- vapply(sys_pgks, function(pkg) system2("dpkg", c("-l", pkg), stdout = NULL, stderr = NULL) == 0,  logical(1))
-if (any(!has_pkgs)) {
-    system2("apt-get", "update")
-    system2("apt-get", c("install", "-y", sys_pgks[!has_pkgs]))
-}
+    ubuntu_version <- v_os_info[['VERSION_ID']]
+    sys_deps_for_pkg <- remotes::system_requirements("ubuntu", ubuntu_version, path = repo_path)
+    sys_pgks <- gsub("^apt-get install -y ", "", sys_deps_for_pkg)
+    has_pkgs <- vapply(sys_pgks, function(pkg) system2("dpkg", c("-l", pkg), stdout = NULL, stderr = NULL) == 0,  logical(1))
+    if (any(!has_pkgs)) {
+        system2("apt-get", "update")
+        system2("apt-get", c("install", "-y", sys_pgks[!has_pkgs]))
+    }
 } else {
-cat(paste("Script not implemented for:", v_os_info[['NAME']])
+    cat(paste("Script not implemented for:", v_os_info[['NAME']]))
 }
 
 
