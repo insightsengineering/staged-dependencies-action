7fb2c40952b99cda070999823592f0e02389c29b
dinakar29 <26552821+dinakar29@users.noreply.github.com>
2021-12-02 08:11:34 -0600

    Delint and sylize R code

--- staged-dependencies.R
+++ staged-dependencies.R
@@ -1,77 +1,95 @@
 #!/usr/bin/env Rscript
 
-split_to_map = function(args){
-    tmp = strsplit(x =unlist(strsplit(args, ",")), "=")
-    content = unlist(lapply(tmp, function(x) x[2]))
-    names(content) = unlist(lapply(tmp, function(x) x[1]))
-    return(content)
+split_to_map <- function(args) {
+  tmp <- strsplit(x = unlist(strsplit(args, ",")), "=")
+  content <- unlist(lapply(tmp, function(x) x[2]))
+  names(content) <- unlist(lapply(tmp, function(x) x[1]))
+  return(content)
 }
 
 repo_path <- Sys.getenv("SD_REPO_PATH", ".")
 staged_version <- Sys.getenv("SD_STAGED_DEPENDENCIES_VERSION", "v0.2.4")
 git_ref <- Sys.getenv("SD_GIT_REF")
 threads <- Sys.getenv("SD_THREADS", "auto")
-cran_repos <- Sys.getenv("SD_CRAN_REPOSITORIES", "CRAN=https://cloud.r-project.org/")
+cran_repos <- Sys.getenv(
+  "SD_CRAN_REPOSITORIES",
+  "CRAN=https://cloud.r-project.org/"
+)
 cran_repos_biomarker <- Sys.getenv("SD_ENABLE_BIOMARKER_REPOSITORIES", "false")
-token_mapping <- Sys.getenv("SD_TOKEN_MAPPING", "https://github.com=GITHUB_PAT,https://gitlab.com=GITLAB_PAT")
+token_mapping <- Sys.getenv(
+  "SD_TOKEN_MAPPING",
+  "https://github.com=GITHUB_PAT,https://gitlab.com=GITLAB_PAT"
+)
 check <- Sys.getenv("SD_ENABLE_CHECK", "true")
 
 cat("\n==================================\n")
-cat(paste("repo_path: \"", repo_path, "\"\n", sep=""))
-cat(paste("staged_version: \"", staged_version, "\"\n", sep=""))
-cat(paste("git_ref: \"", git_ref, "\"\n", sep=""))
-cat(paste("threads: \"", threads, "\"\n", sep=""))
-cat(paste("check: \"", check, "\"\n", sep=""))
-cat(paste("cran_repos: \"", cran_repos, "\"\n", sep=""))
-cat(paste("cran_repos_biomarker: \"", cran_repos_biomarker, "\"\n", sep=""))
-cat(paste("token_mapping: \"", token_mapping, "\"\n", sep=""))
+cat("\nRunning staged dependencies installer\n")
+cat(paste("repo_path: \"", repo_path, "\"\n", sep = ""))
+cat(paste("staged_version: \"", staged_version, "\"\n", sep = ""))
+cat(paste("git_ref: \"", git_ref, "\"\n", sep = ""))
+cat(paste("threads: \"", threads, "\"\n", sep = ""))
+cat(paste("check: \"", check, "\"\n", sep = ""))
+cat(paste("cran_repos: \"", cran_repos, "\"\n", sep = ""))
+cat(paste("cran_repos_biomarker: \"", cran_repos_biomarker, "\"\n", sep = ""))
+cat(paste("token_mapping: \"", token_mapping, "\"\n", sep = ""))
 cat("==================================\n")
 
 setwd(repo_path)
 
 if (cran_repos_biomarker == "true") {
-    options(repos = c(split_to_map(cran_repos), BiocManager::repositories()))
+  options(repos = c(split_to_map(cran_repos), BiocManager::repositories()))
 } else {
-    options(repos = split_to_map(cran_repos))
+  options(repos = split_to_map(cran_repos))
 }
 
 if (threads == "auto") {
-    threads <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
-    cat(paste("Number of cores detected:", threads, "\n\n"))
+  threads <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
+  cat(paste("Number of cores detected:", threads, "\n\n"))
 }
 
 options(
-    staged.dependencies.token_mapping = split_to_map(token_mapping)
+  staged.dependencies.token_mapping = split_to_map(token_mapping)
 )
 
 
 if (file.exists("renv.lock")) {
-    renv::restore()
+  renv::restore()
 } else {
-    remotes::install_deps(dependencies = TRUE, upgrade = "never", Ncpus = threads)
+  remotes::install_deps(dependencies = TRUE, upgrade = "never", Ncpus = threads)
 }
 if (file.exists("staged_dependencies.yaml")) {
-    cat("Install Staged Dependencies\n\n")
-    if (!require("staged.dependencies")) {
-        remotes::install_github("openpharma/staged.dependencies", ref = staged_version, Ncpus = threads, upgrade = "never")
-    }
+  cat("Install Staged Dependencies\n\n")
+  if (!require("staged.dependencies")) {
+    remotes::install_github(
+      "openpharma/staged.dependencies",
+      ref = staged_version,
+      Ncpus = threads,
+      upgrade = "never"
+    )
+  }
 
-    cat(paste("\nCalculating Staged Dependency Table for ref: ", git_ref, "...\n\n"))
+  cat(paste(
+    "\nCalculating Staged Dependency Table for ref: ", git_ref, "...\n\n"
+  ))
 
-    if (git_ref != "" && ! startsWith(git_ref, "refs/pull")){
-        x <- staged.dependencies::dependency_table(ref=git_ref)
-    } else {
-        x <- staged.dependencies::dependency_table()
-    }
+  if (git_ref != "" && !startsWith(git_ref, "refs/pull")) {
+    x <- staged.dependencies::dependency_table(ref = git_ref)
+  } else {
+    x <- staged.dependencies::dependency_table()
+  }
 
 
-    print(x, width = 120)
-    cat("\n\n")
+  print(x, width = 120)
+  cat("\n\n")
 
-    if(check == "true"){
-        cat("\nRunning check yaml consistent...\n\n")
-        staged.dependencies::check_yamls_consistent(x, skip_if_missing_yaml = TRUE)
-    }
+  if (check == "true") {
+    cat("\nRunning check yaml consistent...\n\n")
+    staged.dependencies::check_yamls_consistent(x, skip_if_missing_yaml = TRUE)
+  }
 
-    staged.dependencies::install_deps(dep_structure = x, install_project = FALSE, verbose = TRUE)
+  staged.dependencies::install_deps(
+    dep_structure = x,
+    install_project = FALSE,
+    verbose = TRUE
+  )
 }
--- system-dependencies.R
+++ system-dependencies.R
@@ -3,7 +3,8 @@
 repo_path <- Sys.getenv("SD_REPO_PATH", ".")
 
 cat("\n==================================\n")
-cat(paste("repo_path: \"", repo_path, "\"\n", sep=""))
+cat("\nRunning system dependencies installer\n")
+cat(paste("repo_path: \"", repo_path, "\"\n", sep = ""))
 cat("==================================\n")
 
 
@@ -11,15 +12,32 @@ os_info <- read.csv("/etc/os-release", sep = "=", header = FALSE)
 
 v_os_info <- setNames(os_info$V2, os_info$V1)
 
-if (v_os_info[['NAME']] == "Ubuntu") {
-    ubuntu_version <- v_os_info[['VERSION_ID']]
-    sys_deps_for_pkg <- remotes::system_requirements("ubuntu", ubuntu_version, path = repo_path)
-    sys_pgks <- gsub("^apt-get install -y ", "", sys_deps_for_pkg)
-    has_pkgs <- vapply(sys_pgks, function(pkg) system2("dpkg", c("-l", pkg), stdout = NULL, stderr = NULL) == 0,  logical(1))
-    if (any(!has_pkgs)) {
-        system2("apt-get", "update")
-        system2("apt-get", c("install", "-y", sys_pgks[!has_pkgs]))
-    }
+if (v_os_info[["NAME"]] == "Ubuntu") {
+  ubuntu_version <- v_os_info[["VERSION_ID"]]
+  sys_deps_for_pkg <- remotes::system_requirements(
+    "ubuntu", ubuntu_version,
+    path = repo_path
+  )
+  sys_pgks <- gsub("^apt-get install -y ", "", sys_deps_for_pkg)
+  has_pkgs <- vapply(
+    sys_pgks,
+    function(pkg) {
+      system2(
+        "dpkg",
+        c("-l", pkg),
+        stdout = NULL,
+        stderr = NULL
+      ) == 0
+    },
+    logical(1)
+  )
+  if (any(!has_pkgs)) {
+    system2("apt-get", "update")
+    system2("apt-get", c("install", "-y", sys_pgks[!has_pkgs]))
+  }
 } else {
-    cat(paste("System dependencies not implemented for os:", v_os_info[['NAME']]))
+  cat(paste(
+    "System dependencies not implemented for os:",
+    v_os_info[["NAME"]]
+  ))
 }
